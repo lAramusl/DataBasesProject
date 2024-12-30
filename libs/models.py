6a004dd76d269f882f3e8d7cdcee3f9212d8f115
Aramus <aramusisraelyan@gmail.com>
2024-12-30 23:20:28 +0400

    kinda made the models

---
+++ libs/models.py
@@ -0,0 +1,32 @@
+from sqlalchemy import Column, Integer, String, Float, VARCHAR, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from pydantic import BaseModel
+
+Base = declarative_base()
+
+class Laptop(Base):
+    __tablename__ = "Laptop"
+    id = Column(Integer, primary_key=True, index=True)
+    Model = Column(VARCHAR(20), nullable=False)
+    CPU = Column(VARCHAR(20), nullable=False)
+    GPU = Column(VARCHAR(20), nullable=False)
+    RAM = Column(VARCHAR(20), nullable=False)
+    ScreenSize = Column(VARCHAR(10), nullable=False)
+    Matrix = Column(VARCHAR(10), nullable=False)
+
+class Producer(Base):
+    __tablename__ = "Producer"
+    id = Column(Integer, primary_key=True, index=True)
+    Name = Column(String, nullable=False)
+    Country = Column(String, nullable=False)
+    Placement = Column(String, nullable=True)
+    Warranty = Column(Float, nullable=True)
+
+class MarketOffer(Base):
+    __tablename__ = "MarketOffer"
+    id = Column(Integer, primary_key=True, index=True)
+    LaptopID = Column(Integer, nullable=False)#add foreign key thing
+    ProducerID = Column(Integer, nullable=False)#add foreign key thing
+    Price = Column(Float, nullable=False)
+    Date = Column(DateTime, nullable=False)
