cf8fb29ea602bc4445fd76aa3f1154d527fbcff0
Aramus <aramusisraelyan@gmail.com>
2024-12-30 14:05:19 +0400

    ITS WORKING

--- scripts/init_db.py
+++ scripts/init_db.py
@@ -2,10 +2,15 @@ import psycopg2
 
 def create_database(dbname='aramusDB', user='Aramus', password='1234'):#nobody's gonna hack this
     conn = psycopg2.connect(
-        dbname = dbname,
+        dbname = 'postgres',
         user = user,
         password = password,
+        host = 'localhost',
+        port = '5432'
     )
+
+    conn.autocommit = True
+
     cursor = conn.cursor()
 
     cursor.execute("CREATE DATABASE pc_shop")
