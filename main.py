73ec814c1acbd843ea312fa911ea8cca41b8bda1
Aramus <aramusisraelyan@gmail.com>
2025-01-01 14:57:27 +0400

    crud done

--- main.py
+++ main.py
@@ -14,8 +14,8 @@ Base.metadata.create_all(bind=engine)
 def read_root():
     return {"hello" : "world"}
 
-#LAPTOP CRUD
-@app.post("/laptop/", response_model=sch.LaptopSchema)
+#--------------------------------------------------------------------------------LAPTOP 
+@app.post("/laptops/", response_model=sch.LaptopSchema)
 def create_laptop(laptop: sch.LaptopCreateSchema, db: Session = Depends(get_db)):
     return crud.create_laptop(db=db, laptop=laptop)
 
@@ -23,14 +23,14 @@ def create_laptop(laptop: sch.LaptopCreateSchema, db: Session = Depends(get_db))
 def get_laptops(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     return crud.get_laptops(db=db, skip=skip, limit=limit)
 
-@app.get("/laptop/{laptop_id}", response_model=sch.LaptopSchema)
+@app.get("/laptops/{laptop_id}", response_model=sch.LaptopSchema)
 def get_laptop(laptop_id: int, db: Session = Depends(get_db)):
     db_laptop = crud.find_laptop(db=db, laptop_id=laptop_id)
     if db_laptop is None:
         raise HTTPException(status_code=404, detail="Laptop not found")
     return db_laptop
 
-@app.put('/laptop/{laptop_id}', response_model=sch.LaptopSchema)
+@app.put('/laptops/{laptop_id}', response_model=sch.LaptopSchema)
 def update_laptop(laptop_id: int, laptop: sch.LaptopSchema, db: Session = Depends(get_db)):
     db_laptop = crud.update_laptop(db=db, laptop_id=laptop_id, laptop=laptop)
 
@@ -39,9 +39,69 @@ def update_laptop(laptop_id: int, laptop: sch.LaptopSchema, db: Session = Depend
     
     return db_laptop
 
-@app.delete("/laptop/{laptop_id}", response_model=sch.LaptopSchema)
+@app.delete("/laptops/{laptop_id}", response_model=sch.LaptopSchema)
 def delete_laptop(laptop_id: int, db: Session = Depends(get_db)):
     db_laptop = crud.delete_laptop(db=db, laptop_id=laptop_id)
     if db_laptop is None:
         raise HTTPException(status_code=404, detail="Laptop not found")
     return db_laptop
+
+#--------------------------------------------------------------------------------------PRODUCER
+@app.post("/producers/", response_model=sch.ProducerSchema)
+def create_producer(producer: sch.ProducerCreateSchema, db: Session = Depends(get_db)):
+    return crud.create_producer(db=db, producer=producer)
+
+@app.get("/producers/", response_model=list[sch.ProducerSchema])
+def get_producers(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    return crud.get_producers(db=db, skip=skip, limit=limit)
+
+@app.get("/producers/{producer_id}", response_model=sch.ProducerSchema)
+def get_producer(producer_id: int, db: Session = Depends(get_db)):
+    db_producer = crud.get_producer(db=db, producer_id=producer_id)
+    if db_producer is None:
+        raise HTTPException(status_code=404, detail="Producer not found")
+    return db_producer
+
+@app.put("/producers/{producer_id}", response_model=sch.ProducerSchema)
+def update_producer(producer_id: int, producer: sch.ProducerUpdateSchema, db: Session = Depends(get_db)):
+    db_producer = crud.update_producer(db=db, producer_id=producer_id, producer=producer)
+    if db_producer is None:
+        raise HTTPException(status_code=404, detail="Producer not found")
+    return db_producer
+
+@app.delete("/producers/{producer_id}", response_model=sch.ProducerSchema)
+def delete_producer(producer_id: int, db: Session = Depends(get_db)):
+    db_producer = crud.delete_producer(db=db, producer_id=producer_id)
+    if db_producer is None:
+        raise HTTPException(status_code=404, detail="Producer not found")
+    return db_producer
+
+#-----------------------------------------------------------------------------------------------MARKET_OFFER
+@app.post("/marketoffers/", response_model=sch.MarketOfferSchema)
+def create_market_offer(market_offer: sch.MarketOfferCreateSchema, db: Session = Depends(get_db)):
+    return crud.create_market_offer(db=db, market_offer=market_offer)
+
+@app.get("/marketoffers/", response_model=list[sch.MarketOfferSchema])
+def get_market_offers(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    return crud.get_market_offers(db=db, skip=skip, limit=limit)
+
+@app.get("/marketoffers/{market_offer_id}", response_model=sch.MarketOfferSchema)
+def get_market_offer(market_offer_id: int, db: Session = Depends(get_db)):
+    db_market_offer = crud.get_market_offer(db=db, market_offer_id=market_offer_id)
+    if db_market_offer is None:
+        raise HTTPException(status_code=404, detail="MarketOffer not found")
+    return db_market_offer
+
+@app.put("/marketoffers/{market_offer_id}", response_model=sch.MarketOfferSchema)
+def update_market_offer(market_offer_id: int, market_offer: sch.MarketOfferUpdateSchema, db: Session = Depends(get_db)):
+    db_market_offer = crud.update_market_offer(db=db, market_offer_id=market_offer_id, market_offer=market_offer)
+    if db_market_offer is None:
+        raise HTTPException(status_code=404, detail="MarketOffer not found")
+    return db_market_offer
+
+@app.delete("/marketoffers/{market_offer_id}", response_model=sch.MarketOfferSchema)
+def delete_market_offer(market_offer_id: int, db: Session = Depends(get_db)):
+    db_market_offer = crud.delete_market_offer(db=db, market_offer_id=market_offer_id)
+    if db_market_offer is None:
+        raise HTTPException(status_code=404, detail="MarketOffer not found")
+    return db_market_offer
